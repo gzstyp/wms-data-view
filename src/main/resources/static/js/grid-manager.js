01d6f18abc1537a1dcf782c696c3f61461834828
gzstyp <444141300@qq.com>
2020-05-27 10:40:35 +0800

    改为动态的数据并重新命名

--- src/main/resources/static/js/grid-manager.js
+++ src/main/resources/static/js/grid-manager.js
@@ -6,7 +6,7 @@
 		initDom:function(){
             $('#time').text(this.getCurrentDate());
             thisPage.methods.getMaterialsList();
-            thisPage.methods.progress();
+            thisPage.methods.statistics();
 		},
         getCurrentDate : function(){
             var now = new Date();
@@ -104,16 +104,106 @@
                 $('#table_materials tbody').html(html);
             },
             /*给予jquery的进度条,三列数据*/
-            progress : function(){
-                $(".progress-part-first").animate({ width: "24%" },300);
-                $(".progress-part-second").animate({ width: "28%" },400);
-                $(".progress-part-third").animate({ width: "48%" },500);
-                $("#wz_zd").animate({ width:"10%" },800);
-                $("#wz_fz").animate({ width:"30%" },800);
-                $("#wz_zp").animate({ width:"26%" },800);
-                $("#wz_gzd").animate({ width:"50%" },800);
-                $("#wz_fdj").animate({ width:"90%" },800);
-                $("#wz_yjb").animate({ width:"100%" },800);
+            initProgress : function(){
+                $(".progress-part-danger").animate({width:"24%"},300);
+                $(".progress-part-warning").animate({width:"28%"},400);
+                $(".progress-part-security").animate({width:"48%"},500);
+            },
+            location : function(dom,value){
+                $(dom).animate({ width:value+''},800);
+            },
+            materialsData : function(){
+                var data = [
+                    {
+                        "id" : "ffffffff9dcbdcebffffffff99573f80",
+                        "goods_name" : "折叠类",
+                        "percentage" : "10%",
+                        "danger":"0-5",
+                        "warning":"6-10",
+                        "security":">10",
+                        "stock" : "118件"
+                    },
+                    {
+                        "id" : "ffffffffb71281a2ffffffffd931eb95",
+                        "goods_name" : "棉被类",
+                        "percentage" : "30%",
+                        "danger":"0-5",
+                        "warning":"6-10",
+                        "security":">10",
+                        "stock" : "120件"
+                    },
+                    {
+                        "id" : "ffffffffc43097b8000000003a72e62f",
+                        "goods_name" : "服装类",
+                        "percentage" : "26%",
+                        "danger":"0-5",
+                        "warning":"6-10",
+                        "security":">10",
+                        "stock" : "11件"
+                    },
+                    {
+                        "id" : "ffffffffdae3cd4affffffff9894b623",
+                        "goods_name" : "帐篷类",
+                        "percentage" : "52%",
+                        "danger":"0-5",
+                        "warning":"6-10",
+                        "security":">10",
+                        "stock" : "158件"
+                    },
+                    {
+                        "id" : "ffffffffddf9f51ffffffffff6157ca3",
+                        "goods_name" : "工作灯类",
+                        "percentage" : "15%",
+                        "danger":"0-5",
+                        "warning":"6-10",
+                        "security":">10",
+                        "stock" : "8件"
+                    },
+                    {
+                        "id" : "ffffffffdfaf333c000000001760f4a5",
+                        "goods_name" : "发电机类",
+                        "percentage" : "90%",
+                        "danger":"0-5",
+                        "warning":"6-10",
+                        "security":">10",
+                        "stock" : "185件"
+                    },
+                    {
+                        "id" : "ffffffffe56e8ef0ffffffff912af74a",
+                        "goods_name" : "应急包类",
+                        "percentage" : "100%",
+                        "danger":"0-5",
+                        "warning":"6-10",
+                        "security":">10",
+                        "stock" : "101件"
+                    }
+                ];
+                return data;
+            },
+            statistics : function(){
+                var html = '';
+                var list = thisPage.methods.materialsData();
+                $.each(list,function(index,data){
+                    html += '<tr>';
+                    html += '<td style="width:180px;padding-left:6px;">'+data.goods_name+'</td>';
+                    html += '<td>';
+                    html += '<div class="progress-part" style="margin-top:-10px;margin-left:6px;">';
+                    html += '<span class="progress-part-danger">'+data.danger+'</span>';
+                    html += '<span class="progress-part-warning">'+data.warning+'</span>';
+                    html += '<span class="progress-part-security">'+data.security+'</span>';
+                    html += '<span id="'+data.id+'" class="progress-above"></span>';
+                    html += '</div>';
+                    html += '</td>';
+                    html += '<td style="position:relative;left:340px;">'+data.stock+'</td>';
+                    html += '</tr>';
+                });
+                $('#table_statistics').html(html);//渲染页面
+                thisPage.methods.initProgress();//初始化进度条
+                $.each(list,function(index,data){
+                    console.info(data.id);
+                    console.info(data.percentage);
+                    thisPage.methods.location("#"+data.id,data.percentage);
+                });
             }
         }
 	};
